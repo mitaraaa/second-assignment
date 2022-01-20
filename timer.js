18e3d44be11bd6a3a4cd5de6250d36af22b492aa
zhilkaidarov <rustam030201@gmail.com>
2022-01-20 17:11:15 +0600

    first commit

---
+++ timer.js
@@ -0,0 +1,16 @@
+let timerHTML = document.querySelector("time");
+
+let time = 43200;
+timer = setInterval(function () {
+    let seconds = time % 60;
+    let minutes = time / 60 % 60;
+    let hours = time / 3600 % 60;
+
+    if (time <= 0) {
+        clearInterval(timer);
+    }
+
+    const timeString = `${hours < 10 ? "0" : ""}${Math.trunc(hours)} : ${minutes < 10 ? "0" : ""}${Math.trunc(minutes)} : ${seconds < 10 ? "0" : ""}${Math.trunc(seconds)}`;
+    timerHTML.textContent = timeString;
+    time--;
+}, 1000);
