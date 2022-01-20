63d888ad78d6d8dfcd38a07f6a96cadb623d9421
zhilkaidarov <rustam030201@gmail.com>
2022-01-20 19:40:33 +0600

    xd

--- timer.js
+++ timer.js
@@ -1,18 +1,18 @@
-
-
 let timerHTML = document.querySelector("time");
 
 let time = 43200;
 let timer = setInterval(function () {
-    let seconds = time % 60;
-    let minutes = time / 60 % 60;
-    let hours = time / 3600 % 60;
+	let seconds = time % 60;
+	let minutes = (time / 60) % 60;
+	let hours = (time / 3600) % 60;
 
-    if (time <= 0) {
-        clearInterval(timer);
-    }
+	if (time <= 0) {
+		clearInterval(timer);
+	}
 
-    const timeString = `${hours < 10 ? "0" : ""}${Math.trunc(hours)} : ${minutes < 10 ? "0" : ""}${Math.trunc(minutes)} : ${seconds < 10 ? "0" : ""}${Math.trunc(seconds)}`;
-    timerHTML.textContent = timeString;
-    time--;
+	const timeString = `${hours < 10 ? "0" : ""}${Math.trunc(hours)} : ${
+		minutes < 10 ? "0" : ""
+	}${Math.trunc(minutes)} : ${seconds < 10 ? "0" : ""}${Math.trunc(seconds)}`;
+	timerHTML.textContent = timeString;
+	time--;
 }, 1000);
